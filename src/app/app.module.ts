b7c282d80f7a860b4cc47c950db3016e6690a8f0
Renan Lucena <reenandeveloper@hotmail.com>
2023-03-24 14:46:09 -0300

    adicionar ao github-pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { ServicosComponent } from './servicos/servicos.component';
+import { SobreComponent } from './sobre/sobre.component';
+import { ContatoComponent } from './contato/contato.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    ServicosComponent,
+    SobreComponent,
+    ContatoComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
